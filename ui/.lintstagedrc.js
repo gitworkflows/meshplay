40f485b86f51735fcb2e338056ce97c6eef76ca7
NxPKG <116948796+NxPKG@users.noreply.github.com>
2023-10-19 21:08:05 +0000

    init commit

---
+++ ui/.lintstagedrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    // Lint & Prettify TS and JS files
+    '**/*.(ts|tsx|js|jsx)': (filenames) => [`npm run lint:fix`, `npm run format`],
+  
+    // Prettify only Markdown and JSON files
+    '**/*.(md|json)': (filenames) => `npx prettier --write ${filenames.join(' ')}`,
+  };
+  
